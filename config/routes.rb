a551e6cd69319cf7bc87e13063e903c8fd0179e0
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2017-10-23 20:24:38 +0100

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-   
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :users
   resources :payments
   resources :reservations
@@ -11,7 +15,7 @@ Rails.application.routes.draw do
   resources :credit_cards
   resources :addresses
   resources :customers
-  
+
   root 'travelagent#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
