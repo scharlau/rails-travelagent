29b00a3288144ad3de2b7e3d744f1d95cf2ab718
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2020-10-19 16:19:03 +0100

    fixed exception method name in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :logged_in?
 
   def redirect_if_not_found
-      logger.error "Attempt to access non-existent #{request.controller_name} #{params[:id]}"
+      logger.error "Attempt to access non-existent #{request.controller_class} #{params[:id]}"
       flash[:notice] = 'Sorry, but that doesn\'t exist.'
       redirect_to(cruises_url)
   end
